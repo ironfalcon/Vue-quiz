99d2bc817809934379ea9e00213650ad172c722e
ironfalcon <ironfalcon@yandex.ru>
2021-02-09 01:31:07 +0400

    add dynamic class check answers and btn logic

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -12,27 +12,38 @@
                 v-for="(answer, index) in answers"
                 :key="index"
                 @click="selectAnswer(index)"
-                :class="[selectedIndex === index ? 'selected' : '']"
+                :class="answerClass(index)"
                 >
                     {{ answer }}
                 </b-list-group-item>
             </b-list-group>
 
-            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button 
+                variant="primary"
+                @click="submitAnswer"
+                :disabled="selectedIndex === null || answered"
+            >
+                Submit
+            </b-button>
             <b-button @click="next" variant="success" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
     props: {
         currentQuestion: Object,
-        next: Function
+        next: Function,
+        increment: Function
     },
     data() {
         return{
-            selectedIndex: null
+            selectedIndex: null,
+            correctIndex: null,
+            shuffledAnswers: [],
+            answered: false
         }
     },
     computed: {
@@ -42,9 +53,43 @@ export default {
             return answers
         }
     },
+    watch: {
+        currentQuestion: {
+            immediate: true,
+            handler() {
+                this.selectedIndex = null
+                this.answered = false,
+                this.shuffleAnswers()
+            }
+        }
+    },
     methods: {
         selectAnswer(index) {
             this.selectedIndex = index
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+            this.shuffledAnswers = _.shuffle(answers)
+            this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+        },
+        submitAnswer() {
+            let isCorrect = false
+            if (this.selectedIndex === this.correctIndex) {
+                isCorrect = true
+            }
+            this.answered = true
+            this.increment(isCorrect)
+        },
+        answerClass(index) {
+            let answerClass = ''
+            if (!this.answered && this.selectedIndex === index) {
+                answerClass = 'selected'
+            } else if (this.answered && this.correctIndex === index) {
+                answerClass = 'correct'
+            } else if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+                answerClass = 'incorrect'
+            }
+            return answerClass
         }
     },
     mounted() {
